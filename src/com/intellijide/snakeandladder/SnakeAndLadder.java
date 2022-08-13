09ba797e83dde97b62784ae9f6dcc720ba517fb9
Aniket Shelar <theaniketshelar@gmail.com>
2022-08-13 10:24:19 +0530

    Added: random method to get number

--- src/com/intellijide/snakeandladder/SnakeAndLadder.java
+++ src/com/intellijide/snakeandladder/SnakeAndLadder.java
@@ -1,9 +1,20 @@
 package com.intellijide.snakeandladder;
 
+import java.util.Scanner;
+
 public class SnakeAndLadder {
+    static int player1Position = 0;
+    public static int rollDie(){
+        System.out.println("Please roll a die");
+        int dieNumber = (int)(Math.random() * 10 % 6) + 1;
+        System.out.println("You get : " +dieNumber);
+        return dieNumber;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder game");
         System.out.println("--------------------------------");
-        int playerPosition = 0;
+//        SnakeAndLadder snakenladder = new SnakeAndLadder();
+        rollDie();
+
     }
 }

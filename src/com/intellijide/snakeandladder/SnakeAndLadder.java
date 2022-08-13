59474fda850c79cf0cbe210319f1fea19b67ed0a
Aniket Shelar <theaniketshelar@gmail.com>
2022-08-13 09:53:58 +0530

    Started snake and ladder game

---
+++ src/com/intellijide/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.intellijide.snakeandladder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder game");
+        System.out.println("--------------------------------");
+        int playerPosition = 0;
+    }
+}
